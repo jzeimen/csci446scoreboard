1bd1e8c70db8fbee21ff7667f5fae9eb3cb11398
Joe Zeimen <jzeimen@gmail.com>
2012-04-25 11:56:19 -0600

    after: rails new scoreboard

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Scoreboard::Application.config.secret_token = '9aa5ee7fe666a658b6706470b0354c257d128994fba0e71940dcf98ba8b9fe60d69c3dcbb66768c10bb5f7e938ad9f85d82996c792674c8e574aa4a30ed601be'
